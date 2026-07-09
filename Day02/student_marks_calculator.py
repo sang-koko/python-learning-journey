605678adce1136abb765deafe9c02365608ce52a
sang-koko <sangeethanandhekar@gmail.com>
2026-07-09 17:10:46 +0530

    Add student marks calculator script

---
+++ Day02/student_marks_calculator.py
@@ -0,0 +1,7 @@
+sub1 = int(input("Enter marks of Subject 1: "))
+sub2 = int(input("Enter marks of Subject 2: "))
+sub3 = int(input("Enter marks of Subject 3: "))
+total = sub1 + sub2 + sub3
+average = total / 3
+print("Total Marks =", total)
+print("Average =", average)
