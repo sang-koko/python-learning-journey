74a31dc7eb7a6e984839d4f428ba478e8f5bc70f
sang-koko <sangeethanandhekar@gmail.com>
2026-07-23 14:17:54 +0530

    Add script to print even and odd numbers from a list

---
+++ Day07/even_odd_list.py
@@ -0,0 +1,11 @@
+numbers = [10, 15, 20, 25, 30]
+
+print("Even Numbers")
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+print("Odd Numbers")
+for num in numbers:
+    if num % 2 != 0:
+        print(num)
